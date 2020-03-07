e73703a5b18a48b800f305a25b0eceecf6da2769
Hao Guan <hguandl@gmail.com>
2020-03-08 01:45:57 +0800

    Add python libraries

--- package.sh
+++ package.sh
@@ -3,14 +3,23 @@
 if [ ${TRAVIS} ]; then
   DEPLOY_NAME="ros-${TRAVIS_TAG}"
 else
-  DEPLOY_NAME="ros-$(git describe)"
+  DEPLOY_NAME="ros-$(git describe --tags)"
 fi
 
 if [ -d src ]; then
   find src -name .DS_Store -delete
-  mkdir $DEPLOY_NAME
-  cp -r src $DEPLOY_NAME
-  python3 patch.py $DEPLOY_NAME/src
-  cp build-requirements.txt $DEPLOY_NAME/requirements.txt
-  tar -cJf $DEPLOY_NAME.tar.xz $DEPLOY_NAME
+  mkdir ${DEPLOY_NAME}
+  cp -r src ${DEPLOY_NAME}
+  python3 patch.py ${DEPLOY_NAME}/src
+  cp build-requirements.txt ${DEPLOY_NAME}/requirements.txt
+  tar -cJf ${DEPLOY_NAME}.tar.xz ${DEPLOY_NAME}
+  rm -rf ${DEPLOY_NAME}
+fi
+
+if [ -f build-requirements.txt ]; then
+  mkdir ${DEPLOY_NAME}_pylib
+  pip3 install -r build-requirements.txt --target=${DEPLOY_NAME}_pylib/lib/python3.7
+  mv ${DEPLOY_NAME}_pylib/lib/python3.7/bin ${DEPLOY_NAME}_pylib
+  tar -cJf ${DEPLOY_NAME}_pylib.tar.xz ${DEPLOY_NAME}_pylib
+  rm -rf ${DEPLOY_NAME}_pylib
 fi
