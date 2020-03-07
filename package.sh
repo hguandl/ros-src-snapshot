661363abd98eb68c104956683b02730efdd8e18b
Hao Guan <hguandl@gmail.com>
2020-03-07 22:12:26 +0800

    Split shell scripts

---
+++ package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if [ -d src ]; then
+  tar cJf src.tar.xz src
+fi
--- retrieve.sh
+++ retrieve.sh
@@ -6,14 +6,18 @@ ROS_CONFIGURATION=${ROS_CONFIGURATION:-desktop_full}
 source ./rosdep-venv/bin/activate
 
 if [ ! -d /etc/ros/rosdep/ ]; then
-    sudo rosdep init
+  sudo rosdep init
 fi
 if [ ! -f /etc/ros/rosdep/sources.list.d/00-shim.list ]; then
-    sudo ./shim
+  sudo ./shim.sh
 fi
 rosdep update
 
 rosinstall_generator ${ROS_CONFIGURATION} --rosdistro ${ROS_DISTRO} --deps --tar > ${ROS_DISTRO}-${ROS_CONFIGURATION}.rosinstall
-wstool init -j8 src ${ROS_DISTRO}-${ROS_CONFIGURATION}.rosinstall
 
-tar cJf src.tar.xz src
+if [ -d src ]; then
+  wstool merge -t src ${ROS_DISTRO}-${ROS_CONFIGURATION}.rosinstall
+  wstool update -t src
+else
+  wstool init -j$(nproc) src ${ROS_DISTRO}-${ROS_CONFIGURATION}.rosinstall
+fi
